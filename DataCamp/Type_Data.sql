b5d7dff0bd1c41a4c6b1974637e5bd89edf75225
Athirafd <101001282+Athirafd@users.noreply.github.com>
2022-04-21 23:17:58 +0700

    Create Type_Data.sql

---
+++ DataCamp/Type_Data.sql
@@ -0,0 +1,14 @@
+-- Beberapa tipe data yang common, PostgreSQL
+-- TEXT
+  -- Untuk tipe data string dengan panjang (length) bebas
+-- Varchar()
+  -- Untuk data dengan panjang maksimal ditentukan
+-- Char()
+  -- Untuk data dengan panjang maksimal fix
+  -- EX: NIK, kode pos, NIM, KK, dll
+-- Boolean - True, False, Null
+  -- 
+
+-- TEXT
+  
+-- Varchar()
