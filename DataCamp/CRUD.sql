3adbafedb2755948554208c42c45b069f17d8a9b
Athirafd <101001282+Athirafd@users.noreply.github.com>
2022-04-21 11:39:07 +0700

    Update CRUD.sql

--- DataCamp/CRUD.sql
+++ DataCamp/CRUD.sql
@@ -1,4 +1,4 @@
------ CRUDE
+----- CRUDE ----------
 -- Create
   -- ALTER TABLE
 -- Read
@@ -7,23 +7,35 @@
 
 -- CREATE TABLE
     -- Fungsinya untuk membuat tabel di dalam database
-CREATE TABLE universities (
-    university_shortname text,
-    university text,
-    university_city text
-);
-SELECT * 
-FROM universities;
+    -- Syntax
+    CREATE TABLE universities (
+        university_shortname text,
+        university text,
+        university_city text
+    );
+    SELECT * 
+    FROM universities;
 
 -- Alter Table (ADD, RENAME, DROP)
     -- ADD Fungsinya untuk menambahkan kolom pada sebuah tabel
     -- RENAME mengganti nama kolom
     -- DROP menghilangkan/remove kolom
-ALTER TABLE table_name
-ADD COLUMN column_name_add type_data;
+    -- Syntax
+    ALTER TABLE table_name
+    ADD COLUMN column_name_add type_data;
 
-ALTER TABLE table_name
-RENAME COLUMN column_name_add TO new_column_name;
+    ALTER TABLE table_name
+    RENAME COLUMN column_name_add TO new_column_name;
 
-ALTER TABLE table_name
-DROP COLUMN column_name_add;
+    ALTER TABLE table_name
+    DROP COLUMN column_name_add;
+    
+-- ALTER TABLE/COLUMN (Type Data)
+    -- Fungsinya untuk 
+  
+    
+-- DROP
+    -- Fungsinya untuk menghilangkan tabel, tanpa ALTER
+    -- SYNTAX
+    DROP TABLE table_name;
+
